aaef1bf7ef2f299978bde10d85e4688c403c8d36
Jiajun Fan <celeheaven@gmail.com>
2017-03-14 10:55:05 +0800

    Update entry.go

--- entry.go
+++ entry.go
@@ -2,124 +2,144 @@ package main
 
 import (
 	"encoding/gob"
+	"errors"
 	"fmt"
 	"github.com/hanwen/go-fuse/fuse"
 	"github.com/hanwen/go-fuse/fuse/nodefs"
 	"io"
-	"time"
+	"io/ioutil"
 )
 
-const kDefaultFsNameLength = 16
-const kDefaultFsKeyLength = 16
+const (
+	kMaxEntryNumberInFile = 1024
+)
 
-type FileSystem struct {
-	config  *Config
-	oss     Oss
-	entryId int
-	Name    string
-	entries []*Entry
+type Entry struct {
+	Id       int
+	ParentId int
+	Dir      bool
+	Key      string
+	Name     string
+	FsName   string
+	fs       *FileSystem
+	nodefs.Node
+	size uint64
 }
 
-func NewFileSystem(config *Config) *FileSystem {
-	fs := &FileSystem{}
-	fs.config = config
-	fs.oss = MakeOss(config.Oss)
-	return fs
+func NewDir(id int, name string, parent *Entry) (*Entry, error) {
+	return newEntry(id, name, "", "", parent, true)
 }
 
-func (fs *FileSystem) WriteFileEntries() {
-	var fp io.WriteCloser
-	var fpe Encryptor
-	var enc *gob.Encoder
-	for i := 0; i < len(fs.entries); i++ {
-		if i%fs.config.Fs.BlockSize == 0 {
-			fName := fmt.Sprintf("%s_%d", fs.config.Fs.Prefix, i/fs.config.Fs.BlockSize)
-			println(fName)
-			if fpe != nil {
-				fpe.Close()
-			}
-			if fp != nil {
-				fp.Close()
-			}
-			fp, _ = fs.oss.Create(fName)
-			fpe = MakeEncryptor(fs.config.Enc, fp)
-			enc = gob.NewEncoder(fpe)
+func NewFile(id int, name string, fsName string, key string, parent *Entry) (*Entry, error) {
+	return newEntry(id, name, fsName, key, parent, false)
+}
+
+func newEntry(id int, name string, fsName string, key string, parent *Entry, dir bool) (*Entry, error) {
+	if parent != nil && parent.Dir == false {
+		return nil, errors.New("parent entry can't be file type")
+	}
+	entry := Entry{id, 0, dir, key, name, fsName, nil, nil, 0}
+
+	entry.Id = id
+	if parent == nil {
+		entry.ParentId = 0
+	} else {
+		entry.ParentId = parent.Id
+	}
+	entry.Key = key
+	entry.Dir = dir
+	entry.Name = name
+	entry.FsName = fsName
+	return &entry, nil
+}
+
+func ReadEntries(reader io.Reader, buff []*Entry) error {
+	dec := gob.NewDecoder(reader)
+	return dec.Decode(&buff)
+}
+
+func WriteEntries(writer io.Writer, buff []*Entry) error {
+	enc := gob.NewEncoder(writer)
+	return enc.Encode(buff)
+}
+
+func (e *Entry) Print() {
+	if e.Dir {
+		fmt.Printf("[Dir]: Id: %d, ParentId: %d, Name: %s, FsName: %s\n",
+			e.Id, e.ParentId, e.Name, e.FsName)
+	} else {
+		fmt.Printf("Id: %d, ParentId: %d, Name: %s, FsName: %s, Key %s\n",
+			e.Id, e.ParentId, e.Name, e.FsName, e.Key)
+	}
+}
+
+func (e *Entry) Stat(out *fuse.Attr) {
+	out.Mode = fuse.S_IFREG | 0444
+	if e.size != 0 {
+		out.Size = e.fs.oss.Size(e.FsName)
+		e.size = out.Size
+	} else {
+		out.Size = e.size
+	}
+}
+
+func (e *Entry) Data() (data []byte) {
+	fp, err := e.fs.oss.Open(e.FsName)
+	if err != nil {
+		return nil
+	}
+	fpd := MakeDecryptor(e.fs.config.Enc, fp)
+	data, _ = ioutil.ReadAll(fpd)
+
+	out := fuse.Attr{}
+	e.Stat(&out)
+	size := int(out.Size)
+	if len(data) < size {
+		data = append(data, make([]byte, size-len(data))...)
+	}
+
+	fpd.Close()
+	fp.Close()
+	return
+}
+
+func (n *Entry) OnMount(c *nodefs.FileSystemConnector) {
+	n.fs.onMount()
+}
+
+func (n *Entry) OpenDir(context *fuse.Context) (stream []fuse.DirEntry, code fuse.Status) {
+	children := n.Inode().Children()
+	stream = make([]fuse.DirEntry, 0, len(children))
+	for k, v := range children {
+		mode := fuse.S_IFREG | 0666
+		if v.IsDir() {
+			mode = fuse.S_IFDIR | 0777
 		}
-		entry := fs.entries[i]
-		enc.Encode(entry)
+		stream = append(stream, fuse.DirEntry{
+			Name: k,
+			Mode: uint32(mode),
+		})
 	}
-	if fpe != nil {
-		fpe.Close()
-		fp.Close()
-	}
+	return stream, fuse.OK
 }
 
-func (fs *FileSystem) readFileEntries() {
-	index := 0
-	for {
-		fName := fmt.Sprintf("%s_%d", fs.config.Fs.Prefix, index)
-		index++
-		if err := fs.oss.Stat(fName); err != nil {
-			break
-		} else {
-			fp, _ := fs.oss.Open(fName)
-			fpd := MakeDecryptor(fs.config.Enc, fp)
-			dec := gob.NewDecoder(fpd)
-			for {
-				entry := &Entry{}
-				if err := dec.Decode(entry); err != nil {
-					fpd.Close()
-					fp.Close()
-					break
-				} else {
-					if entry.Id != 0 {
-						entry.fs = fs
-						entry.Node = nodefs.NewDefaultNode()
-						fs.entries = append(fs.entries, entry)
-						if fs.entryId+1 != entry.Id {
-							panic("entry ID error")
-						}
-						entry.Print()
-						fs.entryId = entry.Id
-						entry.fs = fs
-					} else {
-						panic("root should not in database")
-					}
-				}
-			}
-		}
+func (n *Entry) Open(flags uint32, context *fuse.Context) (fuseFile nodefs.File, code fuse.Status) {
+	if flags&fuse.O_ANYWRITE != 0 {
+		return nil, fuse.EPERM
 	}
+
+	return nodefs.NewDataFile(n.Data()), fuse.OK
 }
 
-func (fs *FileSystem) String() string {
-	return fs.Name
+func (n *Entry) Deletable() bool {
+	return false
 }
 
-func (fs *FileSystem) Root() nodefs.Node {
-	return fs.entries[0]
-}
-
-func (fs *FileSystem) onMount() {
-	fs.readFileEntries()
-	for _, entry := range fs.entries {
-		if entry.Id == 0 {
-			continue
-		}
-		pentry := fs.entries[entry.ParentId]
-		println(entry.Name)
-		pentry.Inode().NewChild(entry.Name, entry.Dir, entry)
+func (n *Entry) GetAttr(out *fuse.Attr, file nodefs.File, context *fuse.Context) fuse.Status {
+	if n.Inode().IsDir() {
+		out.Mode = fuse.S_IFDIR | 0777
+		return fuse.OK
 	}
-}
-
-func (fs *FileSystem) Mount() (*fuse.Server, error) {
-	root := &Entry{0, 0, true, "", "", "", fs, nodefs.NewDefaultNode(), 0}
-	fs.entries = append(fs.entries, root)
-	opts := &nodefs.Options{
-		AttrTimeout:  time.Duration(float64(time.Second)),
-		EntryTimeout: time.Duration(float64(time.Second)),
-		Debug:        false,
-	}
-	println(fs.config.Fs.MountPoint)
-	server, _, err := nodefs.MountRoot(fs.config.Fs.MountPoint, root, opts)
-	return server, err
+	n.Stat(out)
+	return fuse.OK
 }
