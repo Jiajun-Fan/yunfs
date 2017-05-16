9a23e7fcfa51279747b2f4419bd969020ab582bc
Jiajun Fan <celeheaven@gmail.com>
2017-05-16 17:08:07 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,16 +8,11 @@ import (
 func main() {
 	SetDebug(DebugDebug)
 	rand.Seed(time.Now().UTC().UnixNano())
-	if config, err := NewConfig(); err != nil {
-		Fatal("no configuration file found, a template is generated at '~/.yunfs/yunfs.json'\n")
+	config := NewConfig()
+	fs := NewFileSystem(config)
+	if server, err1 := fs.Mount(); err1 != nil {
+		Fatal(err1.Error())
 	} else {
-		fs := NewFileSystem(config)
-		//fs.readFileEntries()
-		//fs.WriteFileEntries()
-		if server, err1 := fs.Mount(); err1 != nil {
-			Fatal(err1.Error())
-		} else {
-			server.Serve()
-		}
+		server.Serve()
 	}
 }
